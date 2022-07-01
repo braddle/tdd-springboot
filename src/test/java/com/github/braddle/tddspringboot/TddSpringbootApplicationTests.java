923481d721fbfa8d11dc19eaeb821bfd1404d929
Mark Bradley <braddle@gmail.com>
2022-07-01 10:16:07 +0100

    Project Generation
    Allowing IntelliJ to create the base project.

---
+++ src/test/java/com/github/braddle/tddspringboot/TddSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.braddle.tddspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
