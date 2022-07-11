8f10fb0a95b0085bd50c2d8bd00151864d77b032
Mark Bradley <braddle@gmail.com>
2022-07-11 17:38:27 +0100

    Creating failing healtcheck test

--- src/test/java/com/github/braddle/learning/LearningApplicationTests.java
+++ src/test/java/com/github/braddle/learning/LearningApplicationTests.java
@@ -1,12 +1,34 @@
 package com.github.braddle.learning;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class LearningApplicationTests {
+
+    @Autowired
+    private MockMvc mvc;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void healthCheck() throws Exception {
+        mvc.perform(get("/health")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
